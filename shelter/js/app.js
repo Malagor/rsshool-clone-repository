e583db07e79f3e0ff736215f653e9df4a2387418
Alex Pisarik <seo.dnja@gmail.com>
2020-10-12 23:11:02 +0300

    feat: adaptive main 1200, 768 current version

--- shelter/js/app.js
+++ shelter/js/app.js
@@ -1,36 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-  // mobile menu
-
-  const $body = document.body;
-  const $toggleMenuBtn = document.querySelector('.nav__mobile-toggle');
-  const $menu = document.querySelector('nav .menu');
-  const $nav = document.querySelector('nav');
-  const $header = document.querySelector('header.header');
-
-
-  const mobileMenu = () => {
-    $toggleMenuBtn.classList.toggle('open');
-    $menu.classList.toggle('menu-mobile');
-    $nav.classList.toggle('mobile-open');
-    const headerPosition = getComputedStyle($header).position;
-
-    const isBodyInner = $body.classList.contains('inner-page');
-    console.log(isBodyInner);
-
-
-    if (headerPosition === 'absolute') {
-      $header.style.position = 'fixed'
-    } else {
-      if (isBodyInner) {
-        $header.style.position = 'relative'
-      } else {
-        $header.style.position = 'absolute'
-      }
-
-    }
-  };
-
-  $toggleMenuBtn.addEventListener('click', mobileMenu);
+  //= ../sections/nav/_nav.js
 
 });

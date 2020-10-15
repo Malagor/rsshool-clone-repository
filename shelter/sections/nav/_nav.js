8ad46af6c6b6f27b59aa4fc8d7b46c80e29291fd
Alex Malagor <seo.dnja@gmail.com>
2020-10-15 21:32:11 +0300

    fix: close mobile menu after click on the links

--- shelter/sections/nav/_nav.js
+++ shelter/sections/nav/_nav.js
@@ -1,5 +1,5 @@
 // Mobile menu
-const isInnerPage = document.body.classList.contains('inner-page');
+
 const $toggleMenuBtn = document.querySelector('.nav__mobile-toggle');
 const $menu = document.querySelector('nav .menu');
 const $nav = document.querySelector('nav');
@@ -15,8 +15,32 @@ const mobileMenu = () => {
   if (headerPosition === 'absolute') {
     $header.style.position = 'fixed'
   } else {
-    $header.style.position = isInnerPage ? 'relative' : 'absolute';
+    $header.style.position = '';
   }
 };
-
 $toggleMenuBtn.addEventListener('click', mobileMenu);
+
+
+
+// Закрытие мобильного меню при переходе по ссылке
+
+$menu.addEventListener('click', event => {
+  const target = event.target.closest('.menu__link');
+  if (target) mobileMenu();
+});
+
+
+
+// Обработка скролла страницы
+
+document.body.addEventListener('scroll', function () {
+  let scrollTop = document.body.scrollTop;
+
+  if (scrollTop >= 150) {
+    $header.classList.add('header--scroll');
+  } else {
+    $header.classList.remove('header--scroll');
+  }
+});
+
+

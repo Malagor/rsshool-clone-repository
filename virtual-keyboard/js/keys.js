e0633c92eda881c7f1559d8bc192ff96fe0eef59
Alex Malagor <seo.dnja@gmail.com>
2020-10-28 23:03:02 +0300

    feat: working Shift and doble Shift

---
+++ virtual-keyboard/js/keys.js
@@ -0,0 +1,68 @@
+export const fullKeys = [
+  {
+    eng: '1',
+    ru: '1',
+    shiftEng: '!',
+    shiftRu: '!'
+  },
+  {
+    eng: '2',
+    ru: '2',
+    shiftEng: '@',
+    shiftRu: '"'
+  },
+  {
+    eng: '3',
+    ru: '3',
+    shiftEng: '#',
+    shiftRu: '!'
+  },
+  {
+    eng: '4',
+    ru: '4',
+    shiftEng: '$',
+    shiftRu: '!'
+  },
+  {
+    eng: '5',
+    ru: '5',
+    shiftEng: '%',
+    shiftRu: '!'
+  },
+  {
+    eng: '6',
+    ru: '6',
+    shiftEng: '^',
+    shiftRu: '!'
+  },
+  {
+    eng: '7',
+    ru: '7',
+    shiftEng: '&',
+    shiftRu: '!'
+  },
+  {
+    eng: '8',
+    ru: '8',
+    shiftEng: '*',
+    shiftRu: '!'
+  },
+  {
+    eng: '9',
+    ru: '9',
+    shiftEng: '(',
+    shiftRu: '!'
+  },
+  {
+    eng: '0',
+    ru: '0',
+    shiftEng: ')',
+    shiftRu: '!'
+  },
+  {
+    eng: 'backspace',
+    ru: 'стереть',
+    shiftEng: '',
+    shiftRu: ''
+  },
+];
--- virtual-keyboard/js/script.js
+++ virtual-keyboard/js/script.js
@@ -2,7 +2,9 @@ const Keyboard = {
   elements: {
     main: null,
     keysContainer: null,
-    keys: []
+    keys: [],
+    screen: null,
+    shift: null
   },
 
   eventHandlers: {
@@ -12,17 +14,23 @@ const Keyboard = {
 
   properties: {
     value: '',
-    capsLock: false
+    capsLock: false,
+    shift: false,
+    isLongShift: false,
+    isSelection: false,
+    caretPosition: null,
+    startSelection: null,
+    endSelection: null,
+    timeoutId: null
   },
 
-  init() {
+  init(screen) {
     // Create main elements
     this.elements.main = document.createElement("div");
     this.elements.keysContainer = document.createElement("div");
 
-
     // Setup main elements
-    this.elements.main.classList.add('keyboard', 'keyboard--hidden');
+    this.elements.main.classList.add('keyboard', '111111-----keyboard--hidden');
     this.elements.main.id = 'keyboard';
 
     this.elements.keysContainer.classList.add('keyboard__keys');
@@ -30,32 +38,323 @@ const Keyboard = {
     this.elements.keysContainer.appendChild(this._createKeys());
 
     this.elements.keys = this.elements.keysContainer.querySelectorAll('.keyboard__key');
+    this.elements.shift = this.elements.keysContainer.querySelector('#shift');
+
+    this.elements.screen = document.querySelector(screen);
 
     // Add to DOM
 
     this.elements.main.appendChild(this.elements.keysContainer);
     document.body.appendChild(this.elements.main);
 
-    // Automatically use keyboard for elements with .use-keyboard-input
-    document.querySelectorAll('.use-keyboard-input').forEach(element => {
-      element.addEventListener('focus', () => {
-        this.open(element.value, currentValue => {
-          element.value = currentValue;
-        })
-      })
-    })
+    this._events();
   },
 
-  _createKeys() {
+  _events() {
+    // Automatically use keyboard for elements with .use-keyboard-input
+    this.elements.screen.addEventListener('focus', () => {
+      this._getCaret();
+      this.open(this.elements.screen.value, currentValue => {
+        this.elements.screen.value = currentValue;
+      })
+    });
+
+    window.addEventListener('keydown', ev => {
+      console.log(ev);
+    });
+
+    this.elements.screen.addEventListener('blur', event => {
+      event.preventDefault();
+      this.elements.screen.focus();
+
+    });
+
+    this.elements.screen.addEventListener('click', () => {
+      console.log(this._getCaret());
+    });
+
+    this.elements.screen.addEventListener('mousedown', () => {
+      this._startSelection();
+    });
+
+    this.elements.screen.addEventListener('mouseup', () => {
+      this._endSelection();
+    });
+
+    window.addEventListener('keydown', event => {
+      const code = event.code;
+      if (code === 'ArrowLeft') {
+        this.properties.caretPosition--;
+      } else if (code === 'ArrowRight') {
+        this.properties.caretPosition++;
+      }
+      // console.log(this.properties.caretPosition);
+    });
+  },
+
+  _startSelection() {
+    this.properties.startSelection = this.elements.screen.selectionStart
+  },
+
+  _endSelection() {
+    this.properties.endSelection = this.elements.screen.selectionEnd;
+    this.properties.startSelection = this.elements.screen.selectionStart;
+
+    this.properties.isSelection = this.properties.startSelection !== this.properties.endSelection;
+
+  },
+
+  _createKeys: function () {
 
     const fragment = document.createDocumentFragment();
     const keyLayout = [
       "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
       "q", "w", "e", "r", "t", "y", "u", "i", "o", "p",
       "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", "enter",
-      "done", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?",
-      "space"
+      "shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?",
+      "done", "space"
     ];
+
+    const fullKeys = [
+      {
+        eng: '1',
+        ru: '1',
+        shiftEng: '!',
+        shiftRu: '!'
+      },
+      {
+        eng: '2',
+        ru: '2',
+        shiftEng: '@',
+        shiftRu: '"'
+      },
+      {
+        eng: '3',
+        ru: '3',
+        shiftEng: '#',
+        shiftRu: '!'
+      },
+      {
+        eng: '4',
+        ru: '4',
+        shiftEng: '$',
+        shiftRu: '!'
+      },
+      {
+        eng: '5',
+        ru: '5',
+        shiftEng: '%',
+        shiftRu: '!'
+      },
+      {
+        eng: '6',
+        ru: '6',
+        shiftEng: '^',
+        shiftRu: '!'
+      },
+      {
+        eng: '7',
+        ru: '7',
+        shiftEng: '&',
+        shiftRu: '!'
+      },
+      {
+        eng: '8',
+        ru: '8',
+        shiftEng: '*',
+        shiftRu: '!'
+      },
+      {
+        eng: '9',
+        ru: '9',
+        shiftEng: '(',
+        shiftRu: '!'
+      },
+      {
+        eng: '0',
+        ru: '0',
+        shiftEng: ')',
+        shiftRu: '!'
+      },
+      {
+        eng: 'backspace',
+        ru: 'стереть',
+        shiftEng: '',
+        shiftRu: ''
+      },
+      {
+        eng: 'q',
+        ru: 'й',
+        shiftEng: '',
+        shiftRu: ''
+      },
+      {
+        eng: 'w',
+        ru: 'ц',
+        shiftEng: '',
+        shiftRu: ''
+      },
+      {
+        eng: 'e',
+        ru: 'у',
+        shiftEng: '',
+        shiftRu: ''
+      },
+      {
+        eng: 'r',
+        ru: 'к',
+        shiftEng: '',
+        shiftRu: ''
+      },
+      {
+        eng: 't',
+        ru: 'е',
+        shiftEng: '',
+        shiftRu: ''
+      },
+      {
+        eng: 'y',
+        ru: 'н',
+        shiftEng: '',
+        shiftRu: ''
+      },
+      {
+        eng: 'u',
+        ru: 'г',
+        shiftEng: '',
+        shiftRu: ''
+      },
+      {
+        eng: 'i',
+        ru: 'ш',
+        shiftEng: '',
+        shiftRu: ''
+      },
+      {
+        eng: 'o',
+        ru: 'щ',
+        shiftEng: '',
+        shiftRu: ''
+      },
+      {
+        eng: 'p',
+        ru: 'з',
+        shiftEng: '',
+        shiftRu: ''
+      },
+      {
+        eng: '[',
+        ru: 'х',
+        shiftEng: '',
+        shiftRu: ''
+      },
+      {
+        eng: ']',
+        ru: 'ъ',
+        shiftEng: '',
+        shiftRu: ''
+      },
+      {
+        eng: 'caps',
+        ru: 'caps',
+        shiftEng: '',
+        shiftRu: ''
+      },
+      {
+        eng: 'a',
+        ru: 'ф',
+        shiftEng: '',
+        shiftRu: ''
+      },
+      {
+        eng: 's',
+        ru: 'ы',
+        shiftEng: '',
+        shiftRu: ''
+      },
+      {
+        eng: 'd',
+        ru: 'в',
+        shiftEng: '',
+        shiftRu: ''
+      },
+      {
+        eng: 'f',
+        ru: 'а',
+        shiftEng: '',
+        shiftRu: ''
+      },
+      {
+        eng: 'g',
+        ru: 'п',
+        shiftEng: '',
+        shiftRu: ''
+      },
+      {
+        eng: 'h',
+        ru: 'р',
+        shiftEng: '',
+        shiftRu: ''
+      },
+      {
+        eng: 'j',
+        ru: 'о',
+        shiftEng: '',
+        shiftRu: ''
+      },
+      {
+        eng: 'k',
+        ru: 'л',
+        shiftEng: '',
+        shiftRu: ''
+      },
+      {
+        eng: 'l',
+        ru: 'д',
+        shiftEng: '',
+        shiftRu: ''
+      },
+      {
+        eng: ';',
+        ru: 'ж',
+        shiftEng: ':',
+        shiftRu: ''
+      },
+      {
+        eng: '\'',
+        ru: 'э',
+        shiftEng: '"',
+        shiftRu: ''
+      },
+      {
+        eng: '\\',
+        ru: '\\',
+        shiftEng: '|',
+        shiftRu: '/'
+      },
+      {
+        eng: '',
+        ru: '',
+        shiftEng: '',
+        shiftRu: ''
+      },
+      {
+        eng: '',
+        ru: '',
+        shiftEng: '',
+        shiftRu: ''
+      },
+      {
+        eng: '',
+        ru: '',
+        shiftEng: '',
+        shiftRu: ''
+      },
+
+
+    ];
+
+    // console.log(fullKeys);
 
     // Create HTML for an icon
     const createIconHTML = (icon_name) => {
@@ -78,8 +377,17 @@ const Keyboard = {
           keyElement.innerHTML = createIconHTML('backspace');
 
           keyElement.addEventListener('click', () => {
-            this.properties.value = this.properties.value.substring(0, this.properties.value.length - 1);
+            if (this.elements.screen.selectionStart === this.elements.screen.selectionEnd){
+              this.properties.value = this.properties.value.substring(0, this.properties.value.length - 1);
+              this.properties.caretPosition--;
+
+            } else {
+              this.properties.value = this.properties.value.substring(0, this.elements.screen.selectionStart) + this.properties.value.substring(this.elements.screen.selectionEnd);
+              this.properties.caretPosition = this.elements.screen.selectionStart;
+              this.properties.isSelection = false;
+            }
             this._triggerEvents("oninput");
+
           });
 
           break;
@@ -91,11 +399,28 @@ const Keyboard = {
           keyElement.addEventListener('click', () => {
             this._toggleCapsLock();
             keyElement.classList.toggle('keyboard__key--active');
-            // keyElement.classList.toggle('keyboard__key--active');
-            // this.properties.capsLock.toggle()
           });
 
           break;
+
+        case 'shift':
+          keyElement.classList.add('keyboard__key--wide', 'keyboard__key--activatable');
+          keyElement.id = 'shift';
+          keyElement.innerHTML = createIconHTML('arrow_upward ');
+
+          keyElement.addEventListener('click', () => {
+            this._toggleShift(event);
+            // keyElement.classList.toggle('keyboard__key--active');
+          });
+
+          keyElement.addEventListener('dblclick', () => {
+            this.properties.isLongShift = true;
+            this._toggleShift(event);
+            // keyElement.classList.toggle('keyboard__key--active');
+          });
+
+          break;
+
 
         case 'enter':
           keyElement.classList.add('keyboard__key', 'keyboard__key--wide');
@@ -103,6 +428,7 @@ const Keyboard = {
 
           keyElement.addEventListener('click', () => {
             this.properties.value += '\n';
+            this.properties.caretPosition++;
             this._triggerEvents('oninput');
           });
 
@@ -114,6 +440,7 @@ const Keyboard = {
 
           keyElement.addEventListener('click', () => {
             this.properties.value += ' ';
+            this.properties.caretPosition++;
             this._triggerEvents('oninput');
           });
 
@@ -134,7 +461,7 @@ const Keyboard = {
           keyElement.textContent = key.toLowerCase();
 
           keyElement.addEventListener('click', () => {
-            this.properties.value += this.properties.capsLock ? key.toUpperCase() : key.toLowerCase();
+            this._addSymbolToScreen(key);
             this._triggerEvents('oninput');
           });
 
@@ -149,6 +476,42 @@ const Keyboard = {
     });
 
     return fragment;
+  },
+
+  _addSymbolToScreen(key) {
+    const print = (startCaret, endCaret) => {
+      const startPartStr = this.properties.value.slice(0, startCaret);
+      const lastPartStr = this.properties.value.slice(endCaret);
+      const curKey = (this.properties.capsLock || this.properties.shift) ? key.toUpperCase() : key.toLowerCase();
+
+      if (this.properties.shift) {
+        this.properties.timeoutId = null;
+        // this.elements.shift.classList.toggle('keyboard__key--active');
+        if (!this.properties.isLongShift) {
+          this.properties.shift = false;
+          // this.elements.shift.classList.remove('keyboard__key--active');
+          this._toggleShift();
+        }
+      }
+
+      this.properties.value = startPartStr + curKey + lastPartStr;
+      this.properties.caretPosition++;
+    };
+
+    if (this.properties.isSelection) {
+      print(this.properties.startSelection, this.properties.endSelection);
+    } else {
+      print(this.properties.caretPosition, this.properties.caretPosition);
+    }
+    this.properties.isSelection = false;
+
+    // this.elements.screen.focus();
+    // this.elements.screen.startSelection = this.properties.startSelection;
+    // this.elements.screen.endSelection = this.properties.endSelection;
+
+    // const start = this.elements.screen.selectionStart;
+    // const finish = this.elements.screen.selectionEnd;
+    // print(start, finish);
   },
 
   _triggerEvents(handlerName) {
@@ -167,6 +530,69 @@ const Keyboard = {
     }
   },
 
+  _toggleShift(event) {
+    if (!event) {
+      this.elements.shift.classList.toggle('keyboard__key--active');
+
+      for (const key of this.elements.keys) {
+        if (key.childElementCount === 0) {
+          key.textContent = this.properties.shift ? key.textContent.toUpperCase() : key.textContent.toLowerCase();
+        }
+      }
+      return;
+    }
+
+    const type = event.type;
+
+    if (type === 'click') {
+      console.log('Клик в Click');
+      if (!this.properties.timeoutId) {
+        this.properties.timeoutId = setTimeout(() => {
+          console.log('Внутри setTimeout');
+          this.properties.shift = !this.properties.shift;
+
+          if (this.properties.isLongShift) {
+            this.properties.isLongShift = false;
+          }
+
+          this.elements.shift.classList.toggle('keyboard__key--active');
+
+          for (const key of this.elements.keys) {
+            if (key.childElementCount === 0) {
+              key.textContent = this.properties.shift ? key.textContent.toUpperCase() : key.textContent.toLowerCase();
+            }
+          }
+
+        }, 100);
+        // this.properties.timeoutId = null;
+      }
+    }
+
+    // TODO: Не отрабатывает отключение двойной клик. и не отключается shift после печати символа
+    if (type === 'dblclick') {
+
+      clearTimeout(this.properties.timeoutId);
+      console.log('Внутри dblclick. timeoutId: ', this.properties.timeoutId);
+      this.properties.shift = true;
+      this.properties.isLongShift = true;
+      this.properties.timeoutId = null;
+
+      this.elements.shift.classList.toggle('keyboard__key--active');
+
+      for (const key of this.elements.keys) {
+        if (key.childElementCount === 0) {
+          key.textContent = this.properties.shift ? key.textContent.toUpperCase() : key.textContent.toLowerCase();
+        }
+      }
+    }
+
+
+  },
+
+  _removeShift() {
+
+  },
+
   open(initialValue, oninput, onclose) {
     this.properties.value = initialValue || '';
     this.eventHandlers.oninput = oninput;
@@ -177,12 +603,18 @@ const Keyboard = {
 
   close() {
     this.properties.value = "";
-    this.eventHandlers.oninput = oninput;
-    this.eventHandlers.onclose = onclose;
+    // this.eventHandlers.oninput = oninput;
+    // this.eventHandlers.onclose = onclose;
     this.elements.main.classList.add('keyboard--hidden');
+  },
+
+  _getCaret() {
+    this.properties.caretPosition = this.elements.screen.selectionStart;
+    return this.properties.caretPosition;
   }
 };
 
+
 window.addEventListener('DOMContentLoaded', () => {
-  Keyboard.init();
+  Keyboard.init('#screen');
 });

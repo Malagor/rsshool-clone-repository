4e8401c19238898a3bbc58cc9a800c274e073a09
Alex Malagor <seo.dnja@gmail.com>
2020-11-19 00:58:38 +0300

    refactor: testing create tasks, paged task, highlight elements by hover

--- rs-css/src/js/index.js
+++ rs-css/src/js/index.js
@@ -3,6 +3,12 @@
 
 import '../css/style.css';
 import '../css/style.scss';
+import Task from "./Classes/Task";
+import '../img/GitHub-Mark-64px.png';
+import '../img/rs_school_js.svg';
+import '../img/next-arrow.svg';
+
+const taskList = require('./Models/taskList');
 // import { moduleOne } from './moduleOne';
 //
 // const helloArr = require('./moduleOne.js');
@@ -25,8 +31,6 @@ import '../css/style.scss';
 //
 // console.log(mod);
 //
-//
-//
 // const editor = CodeMirror.fromTextArea(document.getElementById("code"), {
 //   lineNumbers: true, // Нумеровать каждую строчку.
 //   matchBrackets: true,
@@ -38,3 +42,98 @@ import '../css/style.scss';
 // });
 //
 // console.log(editor);
+
+const tasks = taskList.map(taskItem => {
+  const {title, subtitle, syntax, description, examples} = taskItem;
+  const taskObj = {
+    title,
+    description,
+    subtitle,
+    syntax,
+    examples
+  };
+  return Task.create(taskObj)
+});
+
+console.log(tasks);
+
+const $task = document.querySelector('#task');
+$task.innerHTML = "";
+
+// $task.innerHTML = tasks[0].toHTML();
+
+// Листалка задач
+const $arrowLeft = document.querySelector('.arrow__left');
+const $arrowRight = document.querySelector('.arrow__right');
+const $allTaskCount = document.querySelector('#all-tasks-count');
+const $currentTaskNumber = document.querySelector('#current-task');
+
+const countTask = tasks.length;
+let currentTask = 0;
+
+$allTaskCount.textContent = countTask;
+$currentTaskNumber.textContent = currentTask + 1;
+
+function showTask(i) {
+  $currentTaskNumber.textContent = i + 1;
+  $task.innerHTML = "";
+  $task.innerHTML = tasks[i].toHTML();
+}
+
+$task.innerHTML = "";
+$task.innerHTML = tasks[currentTask].toHTML();
+
+$arrowLeft.addEventListener('click', () => {
+  currentTask = currentTask > 0 ? currentTask -= 1 : currentTask;
+  showTask(currentTask);
+});
+
+$arrowRight.addEventListener('click', () => {
+  currentTask = currentTask < countTask - 1 ? currentTask += 1 : currentTask;
+
+  showTask(currentTask);
+});
+
+
+const wagons = document.querySelectorAll('wagon');
+const linesCode = document.querySelectorAll('.html-code div');
+
+function toggleHighlight(el) {
+  const {target} = el;
+  let index;
+
+  if (target.localName === 'wagon') {
+    wagons.forEach((wag, i) => {
+      if (wag === target) {
+        index = i;
+      }
+    });
+  } else {
+    linesCode.forEach((wag, i) => {
+      if (wag === target) {
+        index = i;
+      }
+    });
+  }
+
+  if (index && index >= 0) {
+    wagons[index].classList.toggle('highlight');
+    linesCode[index].classList.toggle('highlight');
+  }
+  // target.classList.toggle('highlight');
+}
+
+function setMouseEvents(arr) {
+  arr.forEach((el) => {
+    el.addEventListener('mouseover', toggleHighlight);
+    el.addEventListener('mouseout', toggleHighlight);
+  });
+}
+
+setMouseEvents(wagons);
+setMouseEvents(linesCode);
+
+
+
+
+

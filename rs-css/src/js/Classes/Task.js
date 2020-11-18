1221a946b9cc867b8bb38c2950b044a42a11850a
Alex Malagor <seo.dnja@gmail.com>
2020-11-19 00:53:23 +0300

    feat: add toHTML() in class Task

--- rs-css/src/js/Classes/Task.js
+++ rs-css/src/js/Classes/Task.js
@@ -1,12 +1,15 @@
 export default class Task {
-  constructor(obj = {}) {
+  constructor(obj) {
     this.id = Task.instanceCount;
     this.target = obj.target;
     this.title = obj.title;
+    this.subtitle = obj.subtitle;
+    this.syntax = obj.syntax;
     this.description = obj.description;
     this.examples = obj.examples;
     this.answers = obj.answers;
     this.level = obj.level;
+    this.code = obj.code;
     this._done = false;
 
     Task.instanceCount += 1;
@@ -22,6 +25,7 @@ export default class Task {
       examples: [],
       answers: [],
       level: 0,
+      code: null,
       ...obj
     };
 
@@ -35,6 +39,23 @@ export default class Task {
   set done(value) {
     this._done = !!value;
     return this;
+  }
+
+  toHTML() {
+    let examples = '';
+    this.examples.forEach(examp => {
+      examples += `<li class="task__example-item">${examp}</li>`
+    });
+    return `
+          <h3 class="task__title">${this.title}</h3>
+          <div class="task__subtitle">${this.subtitle}</div>
+          <div class="task__syntax">${this.syntax}</div>
+          <div class="task__desc">${this.description}
+          </div>
+          <ul class="task__examples-list">
+          ${examples}
+          </ul>
+      `;
   }
 }
 

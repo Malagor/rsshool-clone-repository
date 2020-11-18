ed319db16c787f652fc5a1482ac3e7865d0b8399
Alex Malagor <seo.dnja@gmail.com>
2020-11-18 14:47:24 +0300

    init: basic grid-layout

---
+++ rs-css/src/js/index.js
@@ -0,0 +1,40 @@
+// import CodeMirror from 'codemirror/lib/codemirror';
+// import 'codemirror/lib/codemirror.css';
+
+import '../css/style.css';
+import '../css/style.scss';
+// import { moduleOne } from './moduleOne';
+//
+// const helloArr = require('./moduleOne.js');
+//
+// class TestClass {
+//   constructor() {
+//     const msg = "Using ES2015+ syntax";
+//     console.log(msg);
+//   }
+// }
+//
+// const test = new TestClass();
+// console.log(test);
+//
+// // Пример массива
+// console.log(helloArr);
+//
+// /* пример подключения модуля */
+// const mod = moduleOne(2, 3);
+//
+// console.log(mod);
+//
+//
+//
+// const editor = CodeMirror.fromTextArea(document.getElementById("code"), {
+//   lineNumbers: true, // Нумеровать каждую строчку.
+//   matchBrackets: true,
+//   mode: {name: "javascript", json: true},
+//   indentUnit: 2, // Длина отступа в пробелах.
+//   indentWithTabs: true,
+//   enterMode: "keep",
+//   tabMode: "shift"
+// });
+//
+// console.log(editor);

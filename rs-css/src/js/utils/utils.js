ac95964b2e65ce9ef987ca2b5d7d81efa49cca62
Alex Malagor <seo.dnja@gmail.com>
2020-11-19 20:47:34 +0300

    refactor: create func change index current task

---
+++ rs-css/src/js/utils/utils.js
@@ -0,0 +1,13 @@
+function getNewIndexCurrentTask(cur, max) {
+  let newIndex;
+  if (max) {
+    newIndex = (cur + 1) >= (max - 1) ? max - 1 : cur + 1;
+    return +newIndex;
+  }
+  newIndex = (cur - 1) < 0 ? 0 : cur - 1;
+  return +newIndex;
+}
+
+module.exports = {
+  getNewIndexCurrentTask
+};

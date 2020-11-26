a2a161345680292f88d989fbf612c41265a6e2d2
Alex Malagor <seo.dnja@gmail.com>
2020-11-26 14:15:24 +0300

    refactor: transfer import resourses from class App.js to index.js

--- rs-css/src/js/index.js
+++ rs-css/src/js/index.js
@@ -1,6 +1,13 @@
 import '../css/style.css';
 import '../css/style.scss';
 
+import '../img/GitHub-Mark-64px.png';
+import '../img/rs_school_js.svg';
+import '../img/next-arrow.svg';
+import '../img/check.svg';
+import '../img/rocks-trexture.jpg';
+import '../img/wood-texture.jpg';
+
 import App from "./Controller/App";
 
 App.create('main.main');

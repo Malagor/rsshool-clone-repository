dddae3c1734e8617827bf68b0d53b82ca27b7fd3
Alex Malagor <seo.dnja@gmail.com>
2020-11-26 23:13:44 +0300

    refactor: add textures for elements

--- rs-css/src/js/index.js
+++ rs-css/src/js/index.js
@@ -5,8 +5,11 @@ import '../img/GitHub-Mark-64px.png';
 import '../img/rs_school_js.svg';
 import '../img/next-arrow.svg';
 import '../img/check.svg';
-import '../img/rocks-trexture.jpg';
+import '../img/rocks-texture.png';
 import '../img/wood-texture.jpg';
+import '../img/sand-texture.png';
+import '../img/platform.png';
+import '../img/wagon.png';
 
 import App from "./Controller/App";
 
---
+++ rs-css/src/js/utils/showTooltip.js
@@ -0,0 +1,35 @@
+export function showTooltip(element, top, left) {
+  const tooltip = element;
+  tooltip.style.top = `${top}px`;
+  tooltip.style.left = `${left}px`;
+  tooltip.classList.toggle('open');
+}
+
+export function calculateTooltipsPosition(node) {
+  let top = 0;
+  let left = 0;
+  let offsetTop = 0;
+  let offsetLeft = 0;
+
+  function getPosition(n) {
+    const style = window.getComputedStyle(n);
+    if (style.position === 'absolute') {
+      offsetTop += n.offsetHeight;
+      offsetLeft += n.offsetLeft;
+      const parent = n.parentNode;
+      getPosition(parent);
+    } else {
+      top = n.offsetTop;
+      left = n.offsetLeft;
+    }
+  }
+
+  getPosition(node);
+
+  top -= 70 + offsetTop;
+  left += offsetLeft;
+
+  return {top, left};
+}
+
+

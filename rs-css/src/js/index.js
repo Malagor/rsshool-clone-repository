7f852c783495a99be75e1fab6f22af05a91127e8
Alex Malagor <seo.dnja@gmail.com>
2020-12-06 00:10:22 +0300

    refactor: new textures

--- rs-css/src/js/index.js
+++ rs-css/src/js/index.js
@@ -4,32 +4,37 @@ import '../img/rs_school_js.svg';
 import '../img/next-arrow.svg';
 import '../img/check.svg';
 
+// backgrounds
+import '../img/textures/rw-day.png';
+import '../img/textures/rw-night.png';
+import '../img/textures/bg-day.png';
+import '../img/textures/bg-night.png';
+
 // wagons and platforms
+import '../img/textures/locomotive.png';
 import '../img/textures/platform.png';
-import '../img/textures/platform-blue.png';
-import '../img/textures/platform-green.png';
-import '../img/textures/platform-pink.png';
-import '../img/textures/platform-yellow.png';
-import '../img/textures/wagon.png';
-import '../img/textures/wagon-blue.png';
-import '../img/textures/wagon-green.png';
-import '../img/textures/wagon-purple.png';
-import '../img/textures/wagon-red.png';
+import '../img/textures/platform_blue.png';
+import '../img/textures/platform_green.png';
+import '../img/textures/platform_red.png';
+import '../img/textures/platform_yellow.png';
+import '../img/textures/wagon_blue.png';
+import '../img/textures/wagon_green.png';
+import '../img/textures/wagon_red.png';
+import '../img/textures/wagon_yellow.png';
 
 // goods
-import '../img/textures/rocks-texture.png';
-import '../img/textures/wood-texture.jpg';
-import '../img/textures/sand-texture.png';
-import '../img/textures/tractor-texture.png';
-import '../img/textures/coal-texture.png';
+import '../img/textures/wood_big.png';
+import '../img/textures/wood_small.png';
+import '../img/textures/stones_big.png';
+import '../img/textures/stones_small.png';
+import '../img/textures/coal_big.png';
+import '../img/textures/coal_small.png';
+import '../img/textures/tank_big.png';
+import '../img/textures/tank_small.png';
 
 // components App
-// import '../css/style.css';
 import '../css/style.scss';
-
 import App from "./Classes/App";
 
-App.create('main.main');
-
-
-
+// App.create('main.main');
+App.create();

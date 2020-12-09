21108e52ffedf3ad1de314f18d1b6e37ba9aa551
Alex Malagor <seo.dnja@gmail.com>
2020-12-09 12:56:51 +0300

    refactor: deleted unused code, corrected the color entry

--- rs-css/src/js/utils/typingText.js
+++ rs-css/src/js/utils/typingText.js
@@ -7,9 +7,6 @@ export default function typingText(element, text) {
   const timer = setInterval(() => {
     element.setValue(text.slice(0, i));
     i += 1;
-    // element.selectionStart = 100;
-    // element.selectionEnd = 100;
-    // element.focus();
     sound();
     if (i > text.length) clearInterval(timer);
   }, 100);

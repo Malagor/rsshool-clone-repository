0ea98847dd3df34cb11428699d4ffd67c3427b9f
Alex Malagor <seo.dnja@gmail.com>
2020-12-10 21:36:58 +0300

    feat: status bar

--- covid-dashboard/src/modules/mainApp/mainApp.ts
+++ covid-dashboard/src/modules/mainApp/mainApp.ts
@@ -9,24 +9,27 @@ import Map from '../map/map';
 import MyChar from '../chart/chart';
 import Countries from '../countries/countries';
 import DefaultTemplate from '../default-template/default';
+import Status from '../status/Status';
 
 const elementsDOM = initLayout();
 
-const map = Map(elementsDOM.map);
-const chart = MyChar(elementsDOM.chart);
-const countryComponent = Countries(elementsDOM.countries);
-const defBlock = DefaultTemplate(elementsDOM.table);
+const map = Map(elementsDOM['map']);
+const chart = MyChar(elementsDOM['chart']);
+const countryComponent = Countries(elementsDOM['countries']);
+const defBlock = DefaultTemplate(elementsDOM['table']);
+const status = Status(elementsDOM.status);
 
-function clickTitle() {
+
+function clickTitle(): void {
   console.log('click title Default Block');
 }
 
-function clickImg() {
+function clickImg(): void {
   console.log('click img Default Block');
 }
 
-function renderChart(country = 'All World') {
-  let urlHistorical;
+function renderChart(country: string = 'All World') {
+  let urlHistorical: string;
   if (country === 'All World') {
     urlHistorical = 'https://disease.sh/v3/covid-19/historical/all?lastdays=30';
   } else if (country) {
@@ -37,15 +40,15 @@ function renderChart(country = 'All World') {
       return response.json();
     })
     .then((data) => {
-      let recovered = null;
+      let recovered: object = null;
       if (country && country === 'All World') {
         recovered = data.recovered;
       } else {
         recovered = data.timeline.recovered;
       }
 
-      const label = Object.keys(recovered);
-      const arrData = [];
+      const label: string[] = Object.keys(recovered);
+      const arrData: number[] = [];
 
       label.forEach(key => {
         arrData.push(recovered[key]);
@@ -54,21 +57,23 @@ function renderChart(country = 'All World') {
     });
 }
 
-function changeCountry(country) {
+function changeCountry(country): void {
   if (!country) return;
+
+  status.setCountry(country);
 
   renderChart(country);
 }
 
 function renderMap() {
-  const url = 'https://corona.lmao.ninja/v2/countries';
+  const url: string = 'https://corona.lmao.ninja/v2/countries';
 
   fetch(url)
     .then((response) => {
       return response.json();
     })
     .then((data) => {
-      const infoForMap = [];
+      const infoForMap: object[] = [];
 
       data.forEach(el => {
 

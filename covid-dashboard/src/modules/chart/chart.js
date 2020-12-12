82bf2f9b8be0824ce8e3ef6a7fdee3a4d58246bf
Darya Tsimashenkava (@besovadevka) <timaph@mail.ru>
2020-12-12 10:42:18 +0300

    feat: edit chart's tooltip

--- covid-dashboard/src/modules/chart/chart.js
+++ covid-dashboard/src/modules/chart/chart.js
@@ -78,14 +78,54 @@ export default function MyChar(el) {
           borderColor: 'rgba(255, 255, 255, 1)',
           caretSize: 0,
           titleAlign: 'center',
-          // callbacks: {
-          //   title(context) {
-          //     const titleT =
-          //       document.querySelector('.chart__category').textContent ||
-          //       context.datasets.label;
-          //     return titleT;
-          //   },
-          // },
+          footerAlign: 'center',
+          displayColors: false,
+          callbacks: {
+            title(toolTipItems) {
+              const titleT = toolTipItems[0].label;
+              const year = `20${titleT.slice(-2)}`;
+              const months = [
+                'January',
+                'February',
+                'March',
+                'April',
+                'May',
+                'June',
+                'July',
+                'August',
+                'September',
+                'October',
+                'November',
+                'December',
+              ];
+              const month =
+                titleT[1] === '/' ? titleT.slice(0, 1) : titleT.slice(0, 2);
+              const data =
+                titleT[1] === '/' ? titleT.slice(2) : titleT.slice(3);
+              const day = data[1] === '/' ? data.slice(0, 1) : data.slice(0, 2);
+              const result = `${day}, ${months[+month - 1]} ${year}`;
+              return result;
+            },
+            label(_, data) {
+              const result = `Where: ${data.datasets[0].label}`;
+              return result;
+            },
+            afterLabel(toolTipItems) {
+              const val = toolTipItems.value;
+              if (val > 1000000) {
+                return `Count: ${(val / 1000000).toFixed(1)} m`;
+              }
+              if (val > 1000 && val < 1000000) {
+                return `Count: ${val / 1000} k`;
+              }
+              return `Count: ${val}`;
+            },
+            footer() {
+              const titleT = document.querySelector('.chart__category')
+                .textContent;
+              return titleT;
+            },
+          },
         },
         hover: {
           mode: 'nearest',
@@ -93,6 +133,7 @@ export default function MyChar(el) {
         },
         maintainAspectRatio: false,
         legend: {
+          display: false,
           labels: {
             boxWidth: 0,
           },

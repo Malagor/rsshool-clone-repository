556616eafd7317d47c6bc876fbe4232a6bca2f3a
Darya Tsimashenkava (@besovadevka) <timaph@mail.ru>
2020-12-12 17:05:02 +0300

    fix: change chart funcs

--- covid-dashboard/src/modules/chart/chart.js
+++ covid-dashboard/src/modules/chart/chart.js
@@ -5,18 +5,32 @@ import getChartConfig from './chartConfig';
 
 export default function MyChar(el) {
   el.insertAdjacentHTML('afterbegin', chartHTML);
-  // eslint-disable-next-line no-unused-vars
   let chart = null;
-
+  const chartProps = {
+    isCases: true,
+    isRecovered: false,
+    isDeaths: false,
+  };
+  console.log(chartProps);
   const ctx = document.getElementById('myChart').getContext('2d');
 
-  // function setChart(lbls, dt, title) {
-  //   chart = new Chart(ctx, getChartConfig(lbls, dt, title));
-  //   return chart;
-  // }
+  function getTestData() {
+    const urlHistorical =
+      'https://disease.sh/v3/covid-19/historical/all?lastdays=all';
 
-  function showRecovered(lbls, dt, title) {
-    chart = new Chart(ctx, getChartConfig(lbls, dt, title));
+    fetch(urlHistorical)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const allData = Object.entries(data);
+        console.log(allData);
+      });
+  }
+  getTestData();
+
+  function showRecovered(labels, data, title) {
+    chart = new Chart(ctx, getChartConfig(labels, data, title));
     return chart;
   }
 

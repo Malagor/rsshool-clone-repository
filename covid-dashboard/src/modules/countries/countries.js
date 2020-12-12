75f6817e63825e6334919c206ecdab7e0b6652d0
gryzun33 <yakushevao777@gmail.com>
2020-12-12 11:41:26 +0300

    feat: add html element

--- covid-dashboard/src/modules/countries/countries.js
+++ covid-dashboard/src/modules/countries/countries.js
@@ -1,20 +1,40 @@
 export default function Countries(el) {
-  let getCounrty = null;
+  // let getCounrty = null;
 
-  el.addEventListener('click', (event) => {
-    const { target } = event;
-    if (target === el) return;
+  // el.addEventListener('click', (event) => {
+  //   const { target } = event;
+  //   if (target === el) return;
 
-    getCounrty(target.textContent);
-  });
+  //   getCounrty(target.textContent);
+  // });
 
-  function setClick(fn) {
-    getCounrty = fn;
-  }
+  // function setClick(fn) {
+  //   getCounrty = fn;
+  // }
 
-  return {
-    hendlers: {
-      setClick
-    },
-  };
+  // return {
+  //   handlers: {
+  //     setClick
+  //   },
+  // };
+  el.innerHTML = `<div class="wrapper">
+  <div class="title">Cases by country</div>
+  <input type="text" class="input">
+  <div class="list-countries"></div>
+  <div class="toggle-cases">
+    <button class="btn-cases arrow-left"></button>
+    <div class="cases-title">All Cases</div>
+    <button class="btn-cases arrow-right"></button>
+  </div>
+  <div class="toggle-number">
+    <button class="btn-number arrow-left"></button>
+    <div class="number-title">Absolute number</div>
+    <button class="btn-number arrow-right"></button>
+  </div>
+  <div class="toggle-time">
+    <button class="btn-time arrow-left"></button>
+    <div class="time-title">All time</div>
+    <button class="btn-time arrow-right"></button>
+  </div>
+</div>`;
 }

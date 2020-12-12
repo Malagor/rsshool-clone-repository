d765138599285ea29a4de61c214e88292766a2e0
Darya Tsimashenkava (@besovadevka) <timaph@mail.ru>
2020-12-12 09:10:57 +0300

    feat: edit config and styles of chart

--- covid-dashboard/src/modules/chart/chart.js
+++ covid-dashboard/src/modules/chart/chart.js
@@ -10,7 +10,8 @@ export default function MyChar(el) {
   const ctx = document.getElementById('myChart').getContext('2d');
 
   function showRecovered(lbls, dt, title) {
-    console.log(lbls);
+    // const dataSet = dt.map((it) => (it > 1000 ? it / 1000 : it));
+
     chart = new Chart(ctx, {
       type: 'line',
       data: {
@@ -19,42 +20,81 @@ export default function MyChar(el) {
           {
             label: title,
             data: dt,
-            // backgroundColor: [
-            //   'rgba(255, 99, 132, 0.2)',
-            //   'rgba(54, 162, 235, 0.2)',
-            //   'rgba(255, 206, 86, 0.2)',
-            //   'rgba(75, 192, 192, 0.2)',
-            //   'rgba(153, 102, 255, 0.2)',
-            //   'rgba(255, 159, 64, 0.2)'
-            // ],
-            borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
-              'rgba(255, 159, 64, 1)',
-            ],
-            borderWidth: 2,
+            backgroundColor: 'rgba(255, 0, 0, 0.2)',
+            borderColor: 'rgba(255, 0, 0, 1)',
+            pointBackgroundColor: 'rgba(255, 0, 0, 1)',
+            pointRadius: 1,
+            pointHoverRadius: 5,
+            borderWidth: 0.5,
           },
         ],
       },
       options: {
         scales: {
+          stepSize: 10,
+          xAxes: [
+            {
+              type: 'time',
+              time: {
+                unit: 'month',
+                unitStepSize: 1,
+                displayFormats: {
+                  month: 'MMM YY',
+                },
+              },
+              // ticks: {},
+            },
+          ],
           yAxes: [
             {
               ticks: {
+                maxTicksLimit: 7,
                 beginAtZero: true,
+                callback(value) {
+                  if (value > 1000000) {
+                    return `${value / 1000000} m`;
+                  }
+                  if (value > 1000 && value < 1000000) {
+                    return `${value / 1000} k`;
+                  }
+                  return value;
+                },
+              },
+              gridLines: {
+                display: true,
               },
             },
           ],
         },
         tooltips: {
           mode: 'index',
+          backgroundColor: 'rgba(0, 0, 0, 0.7)',
+          borderWidth: 1,
+          borderColor: 'rgba(255, 255, 255, 1)',
+          caretSize: 0,
+          titleAlign: 'center',
+          callbacks: {
+            title(context) {
+              const titleT =
+                document.querySelector('.chart__category').textContent ||
+                context.datasets.label;
+              return titleT;
+            },
+          },
+        },
+        hover: {
+          mode: 'nearest',
+          intersect: false,
+        },
+        maintainAspectRatio: false,
+        legend: {
+          labels: {
+            boxWidth: 0,
+          },
         },
       },
     });
-    // console.log(chart);
+    console.log(chart);
   }
 
   return {
--- covid-dashboard/src/modules/chart/chartHTML.js
+++ covid-dashboard/src/modules/chart/chartHTML.js
@@ -1,10 +1,14 @@
 import leftButton from '../../assets/img/left_button.svg';
 import rightButton from '../../assets/img/right_button.svg';
 
-const chartHTML = `<canvas id="myChart" height="80%"></canvas>
-  <div class="cart__buttons">
-    <button type="button" class="button">${leftButton}</button>
-    <button type="button" class="button">${rightButton}</button>
+const chartHTML = `
+<button type="button" class="button chart__buttons_changeData">Change data</button>
+<button type="button" class="button chart__buttons_fullScreen">Full</button>
+<div class="chart__wrapper"><canvas id="myChart" style="position: relative; width: 100%; height: 100%;"></canvas></div>
+  <div class="chart__buttons">
+    <button type="button" class="button chart__button">${leftButton}</button>
+    <p class="chart__category"><span>Recovered</span></p>
+    <button type="button" class="button chart__button">${rightButton}</button>
   </div>`;
 
 export default chartHTML;

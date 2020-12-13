5ad139a6fff48e41757311deb168f7ee7a11d8a6
gryzun33 <yakushevao777@gmail.com>
2020-12-13 10:16:58 +0300

    feat: add function renderCountries

--- covid-dashboard/src/modules/countries/countries.js
+++ covid-dashboard/src/modules/countries/countries.js
@@ -1,68 +1,75 @@
-import Queries from '../queries/Queries';
+// import Queries from '../queries/Queries';
+import { createCountriesHTML, createCountriesDOMEelements } from './countriesHTML';
+// import { properties } from '../Properties/Properties';
+
 
 export default function Countries(el) {
-  // let getCounrty = null;
+  createCountriesHTML(el);
+  const countriesElements = createCountriesDOMEelements(el);
+  // console.log('countriesElements', countriesElements);
+  let changeView = null;
+  // const query = Queries();
+  
+  el.addEventListener('click', (event) => {
+    const { target } = event;
+    if (target === el) return;
+    // внести изменения в пропертис
+    changeView();
+  });
 
-  // el.addEventListener('click', (event) => {
-  //   const { target } = event;
-  //   if (target === el) return;
-
-  //   getCounrty(target.textContent, 30);
-  // });
-
-  // function setClick(fn) {
-  //   getCounrty = fn;
-  // }
-
-  // return {
-  //   handlers: {
-  //     setClick
-  //   },
-  // };
-  const query = Queries();
-
-  function renderCountries(data) {
-    console.log(data);
+  function setChangeView(fn) {
+    changeView = fn;
   }
 
+  function createCountryDOMElement(cases, name, flag) {
+    const countryDOMelem = document.createElement('div');
+    countryDOMelem.classList.add('country-item');
+    countryDOMelem.innerHTML = `
+      <div class="country-text">
+        <div class="country-cases">${cases}</div>
+        <div class="country-name">${name}</div>
+      </div>
+      <img src=${flag} alt="flag" class="country-flag" width="30px" height="20px">
+    `; 
+    return countryDOMelem;
+  }
+
+  function renderCountries(countries) {
+    countries.sort((a,b) => b.numbOfCases - a.numbOfCases);
+    countriesElements.list.innerHTML = '';
+    countries.forEach((country) => {
+      const elem = createCountryDOMElement(country.numbOfCases, country.countryName, country.flag)   
+      countriesElements.list.append(elem);  
+    })
+  }
 
   function getTestData() {
-    const url = query.allWorldPerPeriod(5);
-    // console.log(url);
-
+    const url = 'https://disease.sh/v3/covid-19/countries';
     fetch(url)
       .then((response) => {
         return response.json();
-      })
+      }) 
       .then((data) => {
-        // console.log(data);
-        renderCountries(data);
+        return data.map((elem) => {
+          const obj ={
+            countryName: elem.country,
+            numbOfCases: elem.cases,
+            flag: elem.countryInfo.flag,
+          };
+          return obj;
+        });
+      })
+      .then((countriesArray) => {
+        renderCountries(countriesArray);
       });
   }
+
   getTestData();
 
-  el.innerHTML = `<div class="wrapper">
-  <div class="title">Cases by country</div>
-  <input type="text" class="input">
-  <div class="list-countries"></div>
-  <div class="toggle-cases">
-    <button class="btn-cases arrow-left"></button>
-    <div class="cases-title">All Cases</div>
-    <button class="btn-cases arrow-right"></button>
-  </div>
-  <div class="toggle-number">
-    <button class="btn-number arrow-left"></button>
-    <div class="number-title">Absolute number</div>
-    <button class="btn-number arrow-right"></button>
-  </div>
-  <div class="toggle-time">
-    <button class="btn-time arrow-left"></button>
-    <div class="time-title">All time</div>
-    <button class="btn-time arrow-right"></button>
-  </div>
-</div>`;
-
   return  {
+    setHandler: {
+      setChangeView,
+    },
     renderCountries
   }
 

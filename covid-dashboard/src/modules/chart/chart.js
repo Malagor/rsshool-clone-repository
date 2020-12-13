e461ca6dbe36170f6e4e8aeeb1f2ae258a3a234e
Alex Malagor <seo.dnja@gmail.com>
2020-12-13 14:21:13 +0300

    refactor: test multiple charts in chart

--- covid-dashboard/src/modules/chart/chart.js
+++ covid-dashboard/src/modules/chart/chart.js
@@ -14,47 +14,55 @@ export default function MyChar(el) {
       type: 'line',
       data: {
         labels: lbls,
-        datasets: [{
+        datasets: [
+          {
           label: title,
           data: dt,
-          // backgroundColor: [
-          //   'rgba(255, 99, 132, 0.2)',
-          //   'rgba(54, 162, 235, 0.2)',
-          //   'rgba(255, 206, 86, 0.2)',
-          //   'rgba(75, 192, 192, 0.2)',
-          //   'rgba(153, 102, 255, 0.2)',
-          //   'rgba(255, 159, 64, 0.2)'
-          // ],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+
+          ],
           borderColor: [
             'rgba(255, 99, 132, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
           ],
           borderWidth: 1
-        }]
+        },
+          {
+            label: "title",
+            data: dt,
+            backgroundColor: [
+              'rgba(40,255,138,0.2)',
+
+            ],
+            borderColor: [
+              'rgb(22,70,255)',
+            ],
+            borderWidth: 1
+          }
+
+
+        ]
       },
       options: {
         scales: {
           yAxes: [{
             ticks: {
-              beginAtZero: true
-            }
-          }]
+              beginAtZero: true,
+            },
+          }],
         },
         tooltips: {
-          mode: 'index'
+          mode: 'index',
         },
 
-      }
+      },
     });
+
     // console.log(chart);
   }
 
   return {
-    showRecovered
-  }
+    showRecovered,
+  };
 }
 

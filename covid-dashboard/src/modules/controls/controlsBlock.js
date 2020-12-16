36561b809ca8aca67a1892a8b5662461d4e1b720
Darya Tsimashenkava (@besovadevka) <timaph@mail.ru>
2020-12-16 19:58:32 +0300

    fix: change controls fuction

--- covid-dashboard/src/modules/controls/controlsBlock.js
+++ covid-dashboard/src/modules/controls/controlsBlock.js
@@ -1,35 +1,28 @@
-import Settings from '../settings/Settings';
+import { setSettingToggleElement } from '../settings/Settings';
 import params from '../../assets/icons/params.svg';
 import fullScreen from '../../assets/icons/screen-full.svg';
 import closeScreen from '../../assets/icons/screen-close.svg';
 
-const settings = Settings();
+const toggleFullScreen = (value, el) => {
+  if (value) el.innerHTML = closeScreen;
+  if (!value) el.innerHTML = fullScreen;
+  return el;
+};
 
-const getControlsBlockHTML = () => {
-  return `<div class="controls-block">
+const getControlsBlockHTML = (el) => {
+  el.innerHTML += `<div class="controls-block">
   <button type="button" class="controls-block_params">${params}</button>
   <button type="button" class="controls-block_fullscreen">${fullScreen}</button>
 </div>`;
-};
-
-const setClickFullScreen = () => {
-  const fullScreenButton = document.querySelector('.controls-block_fullscreen');
-  fullScreenButton.addEventListener('click', () => {
-    if (fullScreenButton.innerHTML === fullScreen) {
-      fullScreenButton.innerHTML = closeScreen;
-    } else {
-      fullScreenButton.innerHTML = fullScreen;
+  const toggle = el.querySelector('.controls-block-params');
+  setSettingToggleElement(toggle);
+  el.addEventListener('click', (ev) => {
+    const { target } = ev;
+    if (target.closest('.controls-block_fullscreen')) {
+      const element = target.closest('.controls-block_fullscreen');
+      toggleFullScreen(element.innerHTML === fullScreen, element);
     }
-    return fullScreenButton;
   });
 };
 
-const setClickParams = () => {
-  const paramsButton = document.querySelector('.controls-block_params');
-  const controlsBlock = document.querySelector('.controls-block');
-  paramsButton.addEventListener('click', () => {
-    settings.showPopup(controlsBlock);
-  });
-};
-
-export { getControlsBlockHTML, setClickFullScreen, setClickParams };
+export { getControlsBlockHTML };

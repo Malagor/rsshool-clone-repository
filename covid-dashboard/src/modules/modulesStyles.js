56927aa5a9e8558580182256b6374e4e92738de8
Darya Tsimashenkava (@besovadevka) <timaph@mail.ru>
2020-12-16 12:53:38 +0300

    feat: add scss for controls block

--- covid-dashboard/src/modules/modulesStyles.js
+++ covid-dashboard/src/modules/modulesStyles.js
@@ -1,14 +1,12 @@
-import "./mainApp/mainApp.scss";
-import "./header/header.scss";
-import "./status/status.scss";
-import "./cases/cases.scss";
-import "./chart/chart.scss";
-import "./countries/countries.scss";
-import "./table/table.scss";
-import "./map/map.scss";
-import "./settings/settings.scss";
+import './mainApp/mainApp.scss';
+import './header/header.scss';
+import './status/status.scss';
+import './cases/cases.scss';
+import './chart/chart.scss';
+import './countries/countries.scss';
+import './table/table.scss';
+import './map/map.scss';
+import './settings/settings.scss';
+import './controls/controlsBlock.scss';
 
-
-import "./footer/footer.scss";
-
-
+import './footer/footer.scss';

07c85f7e4a46ace5ca77bd39a155699e1d35a668
Alex Malagor <seo.dnja@gmail.com>
2020-12-17 15:44:34 +0300

    feat: Prepared all the necessary requests

--- covid-dashboard/src/modules/queries/Queries.js
+++ covid-dashboard/src/modules/queries/Queries.js
@@ -1,19 +1,83 @@
-export const allWorldPerPeriod = (day = 'all') => {
+/*
+answer
+[
+{
+  "country": "string",
+  "county": "string",
+  "updatedAt": "string",
+  "stats": {
+    "confirmed": 0,
+    "deaths": 0,
+    "recovered": 0
+  },
+  "coordinates": {
+    "latitude": "string",
+    "longitude": "string"
+  },
+  "province": "string"
+}
+]
+ */
+export const urlAllTimeByCountries = () => {
+  return 'https://disease.sh/v3/covid-19/jhucsse'
+};
+
+/*
+ answer
+{
+  "cases": {
+    "date": 0
+  },
+  "deaths": {
+    "date": 0
+  },
+  "recovered": {
+    "date": 0
+  }
+}
+ */
+export const urlAllWorldPerPeriod = (day = 'all') => {
   return `https://disease.sh/v3/covid-19/historical/all?lastdays=${day}`;
 };
 
-export const allWorldDisease = () => {
-  return 'https://disease.sh/v3/covid-19/all';
-};
-
-export const countryDataPerPeriod = (country, day = 'all') => {
+/*
+ answer
+{
+  "country": "string",
+  "province": [
+    "string"
+  ],
+  "timeline": {
+    "cases": {
+      "date": 0
+    },
+    "deaths": {
+      "date": 0
+    },
+    "recovered": {
+      "date": 0
+    }
+  }
+}
+ */
+export const urlCountryDataPerPeriod = (country, day = 'all') => {
   if (country) {
     return `https://disease.sh/v3/covid-19/historical/${country}?lastdays=${day}`;
   }
 
-  return null;
+  throw new Error('Not received the name of the country');
 };
 
-export const getPopulation = () => {
-  return 'https://restcountries.eu/rest/v2/all?fields=name;population;flag';
+/*
+ answer
+[
+{
+"flag": string,
+"name":string,
+"population":number
+}
+]
+ */
+export const urlFlagAndPopulation = () => {
+  return 'https://restcountries.eu/rest/v2/all?fields=name;population;flag'
 };

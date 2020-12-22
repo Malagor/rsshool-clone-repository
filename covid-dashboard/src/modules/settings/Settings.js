6250b284057c57b040b77d5269e10eb1c76472bc
gryzun33 <yakushevao777@gmail.com>
2020-12-23 00:36:56 +0300

    feat: add search in input

--- covid-dashboard/src/modules/settings/Settings.js
+++ covid-dashboard/src/modules/settings/Settings.js
@@ -8,13 +8,21 @@
 
 import settingsHTML from './settingsHTML';
 import { properties, setProperties } from '../Properties/Properties';
-// eslint-disable-next-line import/no-cycle
 import { updateApp } from '../mainApp/updataApp';
+import { createListOfCountries } from './createListOfCountries';
+import { filterInputInPopup } from './fiterInputInPopup';
+import { localStorageCountryList } from '../mainApp/localStorageCountryList';
 
 let popup;
 let form;
 let countryInput;
 let currElem;
+let listOfCountries;
+
+setTimeout(() => {
+  listOfCountries = createListOfCountries();
+  console.log(listOfCountries);
+}, 0);
 
 const showPopup = (el) => {
   currElem = el;
@@ -34,7 +42,14 @@ const getFormData = () => {
   const period = form.period.checked;
   const type = form.type.value;
 
-  country = (country === 'All World' || country === '') ? false : country;
+  // country = (country === 'All World' || country === '') ? false : country;
+  const list = localStorageCountryList(null, 'load').map((elem) => elem.country);
+  if (list.includes(form.country.value) || country === 'All World') {
+    country = (country === 'All World' || country === '') ? false : country;
+  } else {
+    listOfCountries.classList.add('setting__list_hide');
+    return;
+  }
 
   const data = {
     country,
@@ -53,6 +68,29 @@ const createSettings = () => {
   popup = document.querySelector('#popup');
   form = document.forms.settings;
   countryInput = form.country;
+
+  countryInput.addEventListener('click', () => {
+    listOfCountries.classList.remove('setting__list_hide');
+    document.querySelectorAll('.setting-list-item').forEach((item) => { 
+        item.addEventListener('click', () => {
+          countryInput.value = item.innerText;
+          listOfCountries.classList.add('setting__list_hide');
+        });
+      });
+  });
+
+  countryInput.addEventListener('keyup', (e) => {
+    filterInputInPopup(countryInput);
+    const list = localStorageCountryList(null, 'load').map((elem) => elem.country);
+    list.unshift('All World');
+    if (e.code === 'Enter') {
+      if (list.includes(countryInput.value)) {
+        listOfCountries.classList.add('setting__list_hide');
+      } else {
+        countryInput.value = '';
+      }
+    }
+  }); 
 
   form.addEventListener('submit', (event) => {
     event.preventDefault();
